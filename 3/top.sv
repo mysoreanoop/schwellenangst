f67aebe35f33e1d606dccd7544a046d0643222d8
mysoreanoop <mnanoop2014@gmail.com>
2021-11-09 13:19:23 -0800

    renamed tb-> top

--- 3/top.sv
+++ 3/top.sv
@@ -4,7 +4,7 @@ module top(
   logic [63:0] addr, wd, rd, pc;
   logic [31:0] inst;
   logic we, re;
-  cpu xx (clk, rst, addr, we, re, wd, rd, pc, inst);
-  datamem dm(addr, we, re, wd, clk, 4'd6, rd);
+  cpu xx (clk, rst, addr, we, wd, rd, pc, inst);
+  datamem dm(addr, we, 1'b1, wd, clk, 4'd6, rd);
   inst_mem im(pc, inst, clk);
 endmodule
